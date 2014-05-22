6f987628593c2a92ab6ee0694c0ab63e92fae518
Eric Anderle <eric@twilio.com>
2014-05-21 17:28:12 -0700

    add new event row

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    var clock = $("#clock");
+    var addRowButton = $("#addRowButton");
+    var eventTable = $("#eventTable");
+
+    var recordButtonHtml = '<button class="btn btn-danger">Record</button>'
+
     var updateClock = function() {
         var pad = function(i) {
             return (i < 10 ? "0" : "") + i;
@@ -8,12 +14,24 @@ $(document).ready(function() {
         var seconds = date.getSeconds();
         var minutes = date.getMinutes();
         var hours = date.getHours();
-        $("#clock").html(pad(hours) + ":" + pad(minutes) + ":" + pad(seconds));
+        clock.html(pad(hours) + ":" + pad(minutes) + ":" + pad(seconds));
     }
 
-    $("#addRowButton").click(function(e) {
-        var newRow = $("<tr><td>NEW</td></tr>");
-        newRow.appendTo($("#eventTable"));
+    addRowButton.click(function(e) {
+        var newButton = $(recordButtonHtml);
+        var newLabel = $('<label>NEW</label>');
+        var newRow = $('<tr/>');
+
+        [newLabel, newButton].forEach(function(elt, idx, arr) {
+            var col = $('<td/>');
+            elt.appendTo(col);
+            col.appendTo(newRow);
+        });
+        newRow.appendTo(eventTable);
+
+        newButton.click(function(e) {
+            newLabel.html("RECORDED");
+        });
     });
 
     updateClock();
